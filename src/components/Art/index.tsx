1061ed21759f4dc1e38e978cf8901a192057fae7
Ahmad-Zaaza <ahmad94.dev@gmail.com>
2023-06-03 11:43:42 +0400

    add dynamic import for sparkles and theme toggler

--- src/components/Art/index.tsx
+++ src/components/Art/index.tsx
@@ -1,18 +1,19 @@
-import Sparkes from "./Sparkles";
+import dynamic from "next/dynamic";
+const Sparkes = dynamic(() => import("./Sparkles"), { ssr: false });
 
 const Art = () => {
   return (
-    <div className="h-64 relative mt-16">
+    <div className="relative h-64 mt-16">
       <Sparkes>
         <div className="h-16"></div>
       </Sparkes>
-      {/* <span className="left-12  art-blob bg-green-300"></span> */}
-      {/* <span className="animation-delay-2000 art-blob   right-36  bg-indigo-400"></span> */}
-      <span className="animation-delay-2000 art-blob   right-36  bg-red-400"></span>
-      <span className="animation-delay-3000 art-blob  right-52 bg-blue-300"></span>
-      {/* <span className="animation-delay-3000 art-blob  right-52 bg-red-300"></span> */}
-      {/* <span className="animation-delay-4000 art-blob  right-44 bg-yellow-300"></span> */}
-      <span className="animation-delay-5000 art-blob  right-28 bg-green-300"></span>
+      {/* <span className="bg-green-300 left-12 art-blob"></span> */}
+      {/* <span className="bg-indigo-400 animation-delay-2000 art-blob right-36"></span> */}
+      <span className="bg-red-400 animation-delay-2000 art-blob right-36"></span>
+      <span className="bg-blue-300 animation-delay-3000 art-blob right-52"></span>
+      {/* <span className="bg-red-300 animation-delay-3000 art-blob right-52"></span> */}
+      {/* <span className="bg-yellow-300 animation-delay-4000 art-blob right-44"></span> */}
+      <span className="bg-green-300 animation-delay-5000 art-blob right-28"></span>
     </div>
   );
 };
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,7 +4,11 @@ import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import classNames from "classnames";
 import { useMemo } from "react";
-import ThemeToggle from "../ThemeToggle";
+// import ThemeToggle from "../ThemeToggle";
+import dynamic from "next/dynamic";
+
+const ThemeToggle = dynamic(() => import("../ThemeToggle"), { ssr: false });
+
 const Navbar: NextPage = () => {
   const history = useRouter();
 
@@ -12,7 +16,7 @@ const Navbar: NextPage = () => {
     <div className="fixed bg-opacity-25 bg-nav z-[1] backdrop-blur-md  top-0 left-0 right-0">
       <nav className={`container flex items-center ${NAV_MAX_WIDTH} py-4`}>
         <Link href="/">
-          <a className="text-xl hover:no-underline font-bold">Ahmad Zaaza</a>
+          <a className="text-xl font-bold hover:no-underline">Ahmad Zaaza</a>
         </Link>
         <div className="ml-5">
           <LinkItem path={history.pathname} href="/works">
