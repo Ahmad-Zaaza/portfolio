582b89bcd888b3403f898181cf114dc305ad75f8
Ahmad-Zaaza <ahmad94.dev@gmail.com>
2023-06-05 08:39:19 +0400

    add 3 new project thumnnails

--- src/components/WorksPage/ProjectCard.tsx
+++ src/components/WorksPage/ProjectCard.tsx
@@ -18,7 +18,7 @@ const ProjectCard = ({
       <Link href={link} passHref>
         <Image src={thumbnail} alt="title" className="rounded-lg" />
 
-        <h3 className="text-xl font-medium text-center">{title}</h3>
+        <h3 className="mt-4 mb-1 text-xl font-medium text-center">{title}</h3>
         <p className="text-center text-muted font-system">{description}</p>
       </Link>
     </div>
--- src/components/WorksPage/Projects/index.tsx
+++ src/components/WorksPage/Projects/index.tsx
@@ -2,12 +2,33 @@ import Section from "@/components/Section";
 import SectionTitle from "@/components/Section/SectionTitle";
 import sweetatLogo from "../../../../public/images/sweetatlogo.jpg";
 import mrglogo from "../../../../public/images/mrglogo.jpg";
+import danahlogo from "../../../../public/images/danahlogo.jpg";
+import knackuilogo from "../../../../public/images/knackuilogo.jpg";
+import crashmania from "../../../../public/images/crashmania.jpg";
 import ProjectCard from "../ProjectCard";
 const Projects = () => {
   return (
     <Section>
       <SectionTitle>Projects</SectionTitle>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2">
+        <ProjectCard
+          thumbnail={crashmania}
+          description="Official E-commerce website for Al Danah Fisheries in Kuwait"
+          title="CrashMania"
+          link="/works/danah"
+        />
+        <ProjectCard
+          thumbnail={knackuilogo}
+          description="Official E-commerce website for Al Danah Fisheries in Kuwait"
+          title="Knack-UI"
+          link="/works/danah"
+        />
+        <ProjectCard
+          thumbnail={danahlogo}
+          description="Official E-commerce website for Al Danah Fisheries in Kuwait"
+          title="Al Danah Fisheries"
+          link="/works/danah"
+        />
         <ProjectCard
           thumbnail={sweetatLogo}
           description="An online sweets ordering platform active in Kuwait "
@@ -16,7 +37,7 @@ const Projects = () => {
         />
         <ProjectCard
           thumbnail={mrglogo}
-          description="E-commerce platform for MRG Mall active in Kuwait"
+          description="Official E-commerce website for MRG mall in Kuwait"
           title="MRG"
           link="/works/mrg"
         />
